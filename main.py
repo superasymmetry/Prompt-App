b35649e3cd1a00efec8f7b9c404464dc1443c919
superasymmetry <64930215+superasymmetry@users.noreply.github.com>
2023-12-25 11:33:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,17 @@ import os
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
+Path("database.db").touch()
+
+conn = sqlite3.connect("database.db")
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS database (id INTEGER PRIMARY KEY AUTOINCREMENT, title STRING, content TEXT, message TEXT);")
+
+# An example prompt
+# c.execute("INSERT INTO database (title, content, message) VALUES ('Translate', 'from English to Mandarin', 'Translate the following sentence from English to Mandarin.');")
+
+conn.commit()
+conn.close()
+
 if __name__=="__main__":
   app.run(debug=True, port=8080)
