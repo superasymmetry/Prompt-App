8a8e724afc7106ebc7b49ecb92fad7d65a10e1fb
superasymmetry <64930215+superasymmetry@users.noreply.github.com>
2023-12-25 11:30:47 -0500

    Update views.py

--- views.py
+++ views.py
@@ -4,12 +4,15 @@ import requests
 import os
 import json
 import sqlite3
-# import pycurl
 from pathlib import Path
 from urllib.parse import urlencode
 import json
+import base64
 
 views = Blueprint(__name__, "views")
+UPLOAD_FOLDER = './upload'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 @views.route('/', methods=['GET','POST'])
 def home():
@@ -53,11 +56,22 @@ def home():
 @views.route('/prompt', methods = ['GET', 'POST'])
 def prompt():
     if request.method == "POST":
+        print(request.files)
         pt = request.form["pr"]
+
+        if 'picture' in request.files:
+            file = request.files["picture"]
+            filename = os.path.join(UPLOAD_FOLDER, file.filename)
+            print(f"Saving file as: {filename}")
+            file.save(filename)
+
         clicked_item = request.args.get('item')
         content = request.args.get('content')
         message = get_prompt_info(clicked_item,content)
-        # message = 'Translate the following from English to Mandarin:'
+        
+        with open((filename), "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
         url = "https://api.openai.com/v1/chat/completions"
         with open(r'C:\Users\2025130\Downloads\Prompt-Library-main\Prompt-Library-main\config.json') as config_file:
             config = json.load(config_file)
@@ -67,23 +81,34 @@ def prompt():
             "Authorization": api_key,
             "Content-Type": "application/json"
         }
+        
         data = {
+            "model": "gpt-4-vision-preview",
             "messages": [
                 {
-                    "content": f"{message}:{pt}",
-                    "role": "user"
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": f"{message}:{pt} Here is also a supplemental image."
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/png;base64, {encoded_string}"
+                            }
+                        }
+                    ]
                 }
             ],
-            "model": "gpt-4"
+            "max_tokens": 4096
         }
         
-        
+        print("reached")
         response = requests.post(url, json=data, headers=headers).json()
-        completion_text = response['choices'][0]['message']['content']
+        completion_text = response['choices'][0]['message']['content'] 
+        print(completion_text)
 
-        # c=pycurl.Curl()
-        # c.setopt(c.URL, 'https://api.openai.com/v1/chat/completions')
-        # post_data = {'field': 'value'}
         return render_template('form_page.html', response1=completion_text)
     else:
         return render_template("form_page.html")
